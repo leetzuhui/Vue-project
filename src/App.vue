55600e8a8015426d87bf09377b9e951fc5eeb6ca
Lee Tzu-Hui <tzuhui.lee@icloud.com>
2022-08-26 18:02:15 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1 class="title">Created by TZUHUI</h1>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    clickHandler() {},
+  },
+};
 </script>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,140 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+  <div v-if="show">
+    <div class="modal title" ref="title">From Modal</div>
+    <div class="modal msg" ref="msg">{{ msg }}</div>
+  </div>
+  <button @click="clickHandler">Click Me to show what the ref is</button>
+  <br />
+  <button @click="show = true">Show</button>
+  <button @click="show = false">Hide</button>
+  <button @click="show = !show">Toggle Show / Hide</button>
+  <br />
+  <div class="box-wrapper">
+    <div class="box" :class="{ active: (info[0].text != '') }" @mouseover.self="EventHandler">
+      Mouseover (Enter)
+      <span class="info" v-if="info[0].text != ''">{{ info[0].text }}</span>
+    </div>
+    <div class="box" :class="{ active: (info[1].text != '') }" @mouseleave.self="EventHandler">
+      MouseLeave
+      <span class="info" v-if="info[1].text != ''">{{ info[1].text }}</span>
+    </div>
+    <div class="box" :class="{ active: (info[2].text != '') }" @dblclick.self="EventHandler">
+      DoubleClick
+      <span class="info" v-if="info[2].text != ''">{{ info[2].text }}</span>
+    </div>
+    <div class="box" :class="{ active: (info[3].text != '') }" @mousemove.self="EventHandler">
+      MouseMove - See the X, Y value
+      <span class="info" v-if="info[3].text != ''">{{ info[3].text }}</span>
+    </div>
+  </div>
+  <br />
+  <div class="book-list-wrapper">
+    <h2>Book List:</h2>
+    <ul class="book-list">
+      <li v-for="item in books" :key="item.key">
+        <p><b>{{ item.name }}</b> ({{ item.year }}) by <i>{{ item.author }}</i></p>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      show: true,
+      info: [{ text: '', key: 0 },
+      { text: '', key: 0 },
+      { text: '', key: 0 },
+      { text: '' }],
+      books: [
+        { name: 'Divine Songs', year: 1715, author: 'Isaac Watts', key: 1 },
+        { name: 'The Gigantick History of the Two Famous Giants', year: 1730, author: 'Thomas Boreman', key: 2 },
+        { name: 'A Little Pretty Pocket', year: 1744, author: 'John Newbery', key: 3 },
+        { name: 'A Description of Three Hundred Animals', year: 1730, author: 'Thomas Boreman', key: 4 },
+        { name: 'The Governess, or The Little Female Academy', year: 1749, author: 'Sarah Fielding', key: 5 },
+        { name: 'The Parables of Our Lord and Saviour Jesus Chris', year: 1768, author: 'Christopher Smart', key: 6 }
+      ]
+    };
+  },
+  methods: {
+    clickHandler() {
+      if (this.$refs.title)
+        console.log(this.$refs.title);
+
+      if (this.$refs.msg)
+        console.log(this.$refs.msg);
+    },
+    EventHandler(event) {
+      if (event.type == 'mouseover') {
+        this.info[0].key++
+        this.info[0].text = 'mouseover ' + (this.info[0].key == 1 ? '' : this.info[0].key)
+      }
+      else if (event.type == 'mouseleave') {
+        this.info[1].key++
+        this.info[1].text = 'mouseleave ' + (this.info[1].key == 1 ? '' : this.info[1].key)
+      }
+      else if (event.type == 'dblclick') {
+        this.info[2].key++
+        this.info[2].text = 'dblclick ' + (this.info[2].key == 1 ? '' : this.info[2].key)
+      }
+      else if (event.type == 'mousemove') {
+        this.info[3].text = 'offsetX: ' + event.offsetX + ', offsetY: ' + event.offsetY;
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss" scoped>
+.modal {
+  font-size: 25px;
+  color: #25c141;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.box-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+
+  .box {
+    max-width: 700px;
+    min-width: 700px;
+    max-height: 350px;
+    margin: 50px auto;
+    padding: 150px 200px;
+    background-color: burlywood;
+    border-radius: 30px;
+    text-align: center;
+    font-size: 30px;
+
+    .info {
+      display: block;
+      margin-top: 15px;
+      font-size: 15px;
+      font-weight: 200;
+    }
+  }
+
+  .active {
+    background-color: crimson;
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.book-list-wrapper {
+  text-align: left;
+  font-size: 2.0rem;
+
+  .book-list {
+    font-size: 1.2rem;
+  }
 }
 </style>
