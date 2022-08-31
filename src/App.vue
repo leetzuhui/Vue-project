1e49bf5f108d2e92674f2aaa18ecd8d4ec769ce2
Lee Tzu-Hui <tzuhui.lee@icloud.com>
2022-08-31 16:31:08 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -27,22 +27,49 @@
       <SlotCom />
     </template>
   </Slot>
+  <PropsAndEmit PES="Static Props" :PED="PED" :PEN="PEN" :PEO="PEO" @NameFromDerived="InvokedInBased"
+    @ApperanceMode="ToggleApperanceMode" :ApperanceMode="ApperanceMode" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
 import Slot from './components/Slot'
 import SlotCom from './components/SlotCom'
+import PropsAndEmit from './components/PropsAndEmit.vue'
 
 export default {
   name: "App",
   components: {
     HelloWorld,
     Slot,
-    SlotCom
+    SlotCom,
+    PropsAndEmit
+  },
+  data() {
+    return {
+      ApperanceMode: true,// true for light, false for dark
+      PED: 'Dynamic Props',
+      PEN: 1984,
+      PEO: {
+        name: 'Tzu-Hui Lee',
+        age: 25,
+        job: 'Developer'
+      }
+    }
   },
   methods: {
-    UnusedHandler() { },
+    InvokedInBased(e) {
+      alert('Name: ' + e.name + '\nModel: ' + e.model + '\nStroage: ' + e.stroage + 'GB\nPrice: $' + e.price)
+    },
+    ToggleApperanceMode() {
+      document.body.classList.toggle('dark')
+      if (this.ApperanceMode)
+        // light
+        this.ApperanceMode = false
+      else
+        // dark
+        this.ApperanceMode = true
+    }
   },
 };
 </script>
---
+++ src/components/PropsAndEmit.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="pe-wrapper">
+    <h2>Props and Emit</h2>
+    <h3 class="sub-title props">Props</h3>
+    <p>String: {{ PES }}</p>
+    <p>String: {{ PED }}</p>
+    <p>Number: {{ PEN }}</p>
+    <p>Object: {{ PEO }}</p>
+    <p>Object: {{ iPhone }}</p>
+    <h3 class="sub-title emit">Emit</h3>
+    <button @click="ClickMe">Click Me to Invoke the method defined in the <b>PropsAndEmit.vue</b></button>
+    <br />
+    <p>Current Apperance Mode: {{ ApperanceMode ? 'Light' : 'Dark' }}</p>
+    <button @click="ToggleApperanceMode">Toogle Light / Dark Mode</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PropsAndEmit',
+  props: {
+    PES: String,
+    PED: String,
+    PEN: Number,
+    PEO: Object,
+    ApperanceMode: Boolean
+  },
+  data() {
+    return {
+      iPhone: { name: 'iPhone 14', model: 'A3642', stroage: 256, price: 899 }
+    }
+  },
+  methods: {
+    ClickMe() {
+      this.$emit('NameFromDerived', this.iPhone)
+    },
+    ToggleApperanceMode() {
+      this.$emit('ApperanceMode')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.pe-wrapper {
+  h2 {
+    font-size: 2.5rem;
+    font-weight: bold;
+  }
+}
+</style>
